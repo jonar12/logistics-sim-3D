e9f00b83c176e00de5b74d0e3e51805b315477dc
jonar12 <a01737788@tec.mx>
2024-11-18 20:30:07 -0600

    Added 3D-bin-packing files (effective)

---
+++ 3D-bin-packing/Caja.py
@@ -0,0 +1,6 @@
+class Caja:
+    def __init__(self, id, width, height, depth):
+        self.id = id
+        self.width = width
+        self.height = height
+        self.depth = depth
---
+++ 3D-bin-packing/Contenedor.py
@@ -0,0 +1,6 @@
+class Contenedor:
+    def __init__(self, id, width, height, depth):
+        self.id = id
+        self.width = width
+        self.height = height
+        self.depth = depth
