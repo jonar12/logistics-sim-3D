ac9df3367dfa916941fdf698f60c8f756fc0d7f5
jonar12 <a01737788@tec.mx>
2024-11-21 19:37:36 -0600

    Usamos los tamanos de cajas establecidos a una escala 1:10

--- Julia/data.jl
+++ Julia/data.jl
@@ -1,21 +1,29 @@
+box_types = [
+    (1, 1, 1),
+    (5, 5, 5),
+    (7, 7, 7)
+]
+
 function generate_sample_data(num_boxes::Int)
-    # Generate container data
+    # Contenedor de 3.3 m2 (capacidad de la Peugeot Partner) (a escala de 10 m2)
     contenedor = Dict(
         "id" => 1,
-        "width" => 20,
-        "height" => 20,
-        "depth" => 20
+        "width" => 15,
+        "height" => 10,
+        "depth" => 22
     )
 
-    # Generate random boxes
     cajas = [
-        Dict(
-            "id" => i,
-            "width" => rand(1:10),
-            "height" => rand(1:10),
-            "depth" => rand(1:10)
-        ) for i in 1:num_boxes
+        let selected_box = rand(box_types)
+            Dict(
+                "id" => i,
+                "width" => selected_box[1],
+                "height" => selected_box[2],
+                "depth" => selected_box[3]
+            )
+        end for i in 1:num_boxes
     ]
+
 
     # Combine into a single dictionary
     data = Dict(
